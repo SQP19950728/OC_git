4c6a6bb696af34b1ad72d0e55064cbc401695c8a
China-SQP <shiqingpu@macbook-pro.local>
2016-05-31 09:27:34 +0800

    Initial Commit

---
+++ OC_git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC_git
+//
+//  Created by China-SQP on 16/5/31.
+//  Copyright © 2016年 China-SQP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OC_git/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OC_git
+//
+//  Created by China-SQP on 16/5/31.
+//  Copyright © 2016年 China-SQP. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
